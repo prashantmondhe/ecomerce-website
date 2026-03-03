caf9ce553247922bc37d2ada60020c9738298e91
prashantmondhe <mondheprashant@gmail.com>
2026-03-03 18:13:36 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+
+<h1> hello </h1>
+
+const App = () => {
+  return (
+<main>
+  <Navbar />
+</main>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="sticky top-0 bg-white z-10 ">
+      <div className=" container hidden lg:block "> 
+        <div className="flex justify-between items-center p-8">
+            <h1 className="text-4xl font-medium">Logo</h1>
+            <div className="relative w-full max-w-[500px]">
+                <input className="bg-[#f2f3f5] border-none outline-none px-6 py-3 rounded-[30px] w-full" type ="text" placeholder= " Search Product..." />
+                 </div>
+        </div>
+      </div>
+    </div>
+    
+
+  )
+}
+
+export default Navbar
