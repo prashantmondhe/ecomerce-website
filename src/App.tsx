8c87c214eb80d6f41e6577b23784a0ddbc5a5068
prashantmondhe <mondheprashant@gmail.com>
2026-03-18 20:03:41 +0530

    seventeenth commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,24 @@
-// import { useState } from 'react';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// // ग्लोबल कंम्पोनंट्स
-// import Navbar from './Components/Navbar/Navbar';
-// import MobNavbar from './Components/MobNavbar/MobNavbar';
-// import Footer from './Components/Footer/Footer';
-// import Cart from './Components/Cart/Cart'; 
-// import Login from './Components/Login/Login';
-// import { CartContextProvider } from './Components/Context/CartContext';
-
-// // पेजेस
-// import Home from './Components/Home/Home';
-// import SearchPage from './Components/SearchPage/SearchPage';
-// import ProductDetails from './Components/ProductDetails/ProductDetails'; 
-
-// const App = () => {
-//   const [showCart, setShowCart] = useState(false);
-//   const [showLogin, setShowLogin] = useState(false); 
-
-//   return (
-//     <CartContextProvider>
-//       <BrowserRouter>
-//         <main className="relative flex flex-col min-h-screen">
-          
-//           <Navbar setShowCart={setShowCart} setShowLogin={setShowLogin} />
-//           <MobNavbar setShowCart={setShowCart} setShowLogin={setShowLogin} />
-          
-//           <div className="flex-grow">
-//             <Routes>
-//               <Route path="/" element={<Home />} />
-//               <Route path="/search" element={<SearchPage />} />
-//               <Route path="/product/:id" element={<ProductDetails />} />
-//             </Routes>
-//           </div>
-
-//           <Footer />
-          
-//           {showLogin && <Login setShowLogin={setShowLogin} />}
-//           {showCart && <Cart setShowCart={setShowCart} />}
-          
-//         </main>
-//       </BrowserRouter>
-//     </CartContextProvider>
-//   );
-// }
-
-// export default App;
 
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// ग्लोबल कंम्पोनंट्स
+
 import Navbar from './Components/Navbar/Navbar';
 import MobNavbar from './Components/MobNavbar/MobNavbar';
 import Footer from './Components/Footer/Footer';
 import Cart from './Components/Cart/Cart'; 
 import Login from './Components/Login/Login';
 
-// Contexts
-import { CartContextProvider } from './Components/Context/CartContext';
-import { WishlistContextProvider } from './Components/Context/WishlistContext'; // नवीन Import
 
-// पेजेस
+import { CartContextProvider } from './Components/Context/CartContext';
+import { WishlistContextProvider } from './Components/Context/WishlistContext'; 
+
+
 import Home from './Components/Home/Home';
 import SearchPage from './Components/SearchPage/SearchPage';
 import ProductDetails from './Components/ProductDetails/ProductDetails'; 
-import WishlistPage from './Components/WishlistPage/WishlistPage'; // नवीन Import
+import WishlistPage from './Components/WishlistPage/WishlistPage'; 
 
 const App = () => {
   const [showCart, setShowCart] = useState(false);
@@ -73,7 +26,7 @@ const App = () => {
 
   return (
     <CartContextProvider>
-      <WishlistContextProvider> {/* <--- संपूर्ण ॲपला Wishlist च्या आत टाकले */}
+      <WishlistContextProvider> 
         <BrowserRouter>
           <main className="relative flex flex-col min-h-screen">
             
@@ -85,7 +38,7 @@ const App = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/search" element={<SearchPage />} />
                 <Route path="/product/:id" element={<ProductDetails />} />
-                {/* विशलिस्टसाठी नवीन राऊट */}
+              
                 <Route path="/wishlist" element={<WishlistPage />} /> 
               </Routes>
             </div>
